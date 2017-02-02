da9396e12c9d469d63041c1fcdb188f066a2bfac
oleno <pirit.lena@gmail.com>
2017-02-02 22:11:26 +0200

    drawing stars

--- game.js
+++ game.js
@@ -14,3 +14,13 @@ window.addEventListener("load" ,function() {
 	stars.height = Game.height;
 
 	var startx = stars.getContext("2d");
+
+	// Drawing stars (draw a brunch of random 2 pixel rectangles onto the offscreen canvas)
+	starCtx.fillStyle = "#FFF";
+	starCtx.globalAlpha = opacity;
+	for (var i = 0, i < numStars; i++) {
+		starCtx.fillRect(Math.floor(Math.random() * stars.width),
+						 Math.floor(Math.random() * stars.height),
+						 2,
+						 2);	
+	}
